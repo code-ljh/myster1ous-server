3ac3049a850eadc2a4834ffb645475c94b872ea0
code-ljh <17720466@qq.com>
2025-09-15 20:23:18 +0800

    Template Done

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
 import * as libTemplate from '#library/template.js';
-import * as libDatabase from '#library/database.js';
 
 export default async function handler(request, response) {
-    var data = await libDatabase.readDb();
-    console.log(data);
     return response.status(200).send(libTemplate.get("home"));
 }
